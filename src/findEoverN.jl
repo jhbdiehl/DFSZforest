f2e4b67797388e03279cece683f5fed1cb69b52d
Johannes Diehl <jhb.diehl@protonmail.com>
2022-01-19 16:52:57 +0100

    First steps to generalize, quads try outs

--- src/findEoverN.jl
+++ src/findEoverN.jl
@@ -9,49 +9,101 @@ using BenchmarkTools
 using StatsPlots
 using Plots
 
-@variables u1::Int u2::Int d1::Int e1::Int
+@variables u1::Int u2::Int d1::Int l1::Int
 
-#Todo: implement path to nD4Final here!
 
-nD4Final = [-2u1 + 2u2, d1-e1-u1+u2, -d1+e1-u1+u2, d1-u1+ 2u2, e1-u1+ 2u2, 2d1-2*e1, 2d1-e1+u2, u2+2*e1-d1, 2d1+ 2u2, d1+e1+ 2u2, 2u2+2*e1, 2d1-e1+u1, u1-d1+ 2*e1, 2d1+u1+u2, d1+e1+u1+u2, u1+2*e1+u2, d1+ 2u1 - u2, e1+ 2u1-u2, d1+e1+2u1, 2u1+2*e1]
+using PyCall
 
 @time begin
+py"""
+import numpy as np
+import sympy as sy
+
+u1, u2, d1, e1 = sy.symbols('u1 u2 d1 e1')
+
+def condition1(u1, u2, d1, e1):
+    return u1 + u2 - d1 - e1
+
+def condition2(u1, u2, d1, e1):
+    return np.abs(u1) + np.abs(u2) + np.abs(d1) + np.abs(e1) - 4
+
+res = sy.nonlinsolve([condition1(u1, u2, d1, e1), condition2(u1, u2, d1, e1), u1+u2+e1+d1 >= 0], [u1, u2, d1, e1])
+print(res)
+"""
+end
+
+
+using NLsolve
+
+# 1: u1, 2: u2, 3:d1, 4:e1
+function f!(Q, x)
+    Q[1] = x[3] + x[4] - x[2]
+
+end
+
+
+Symbolics.solve_for([u1+d1 ~ 2, -2u1+3d1+3*e1-u2 ~ 0], [e1, u2])
+
+quads = [-2u1 + 2u2, d1-l1-u1+u2, -d1+l1-u1+u2, d1-u1+ 2u2, l1-u1+ 2u2, 2d1-2*l1, 2d1-l1+u2, u2+2*l1-d1, 2d1+ 2u2, d1+l1+ 2u2, 2u2+2*l1, 2d1-l1+u1, u1-d1+ 2*l1, 2d1+u1+u2, d1+l1+u1+u2, u1+2*l1+u2, d1+ 2u1 - u2, l1+ 2u1-u2, d1+l1+2u1, 2u1+2*l1]
+
+#@time begin
+
+function EoverN(ups, downs, leptons)
+    2//3 + 2 * simplify_fractions((sum(ups)+sum(leptons)) //(sum(ups)+sum(downs)))
+end
+
+function checks(nH::Int64, model::Vector{Num})
+    length(model) == 9 ? nothing : throw(AssertionError("model does not specify couplings to all SM fermions"))
+    lm = length(unique(model))
+    lm == nH ? nothing : throw(AssertionError("model does not specify nH different hidings, but instead $lm"))
+end
+
+nH = 4
+model = [u1, u2, u1, d1, d1, d1, l1, l1, l1]
+checks(nH, model) #Stop user right there if definition is nonsense.
+
+notu1d1 = isequal.(unique(model),u1) .+ isequal.(unique(model),d1) .== false
 
 # Comes from Combinatorics package
 # Equivalent to Mathematica Subsets
-nD4Subsets = collect(powerset(nD4Final, 2,2));
+subsets = collect(powerset(quads, nH-2,nH-2));
 
 # Below would be way faster when specifying data type for Array{Any}. However "SymbolicUtils.Sym{Real, Base.ImmutableDict{DataType, Any}}" for some reason doesnt work!
 # Equivalent to Mathematica Solve
-nD4sol = Array{Num}(undef, 2, length(nD4Subsets))
+sol = Array{Num}(undef, nH-2, length(subsets))
 errors = 0
 infties = 0
-for i in range(1, length(nD4Subsets))
+for i in range(1, length(subsets))
     try
-        nD4sol[:,i] = Symbolics.solve_for(nD4Subsets[i], [e1,u2])
+        sol[:,i] = Symbolics.solve_for(subsets[i], [l1, u2])
     catch
         errors += 1
-        nD4sol[:,i] = [NaN, NaN]
+        sol[:,i] = [NaN for i in 1:nH-2]
     end
 end
+errors
 
+sol
 
-nD4AnomalyRatio = Array{Num}(undef,size(nD4sol)[2]);
-for i in range(1,size(nD4sol)[2])
-    try nD4AnomalyRatio[i] = EoverN(u1, nD4sol[2,i], d1, nD4sol[1,i])
+AnomalyRatio = Array{Num}(undef,size(sol)[2]);
+for i in range(1,size(sol)[2])
+    try 
+        mnew = deepcopy(model)
+        for j in 1:length(unique(model))-2
+            mnew[isequal.(model,unique(model)[notu1d1][j])] .= sol[j,i]
+        end
+        AnomalyRatio[i] = EoverN(mnew[1:3], mnew[4:6], mnew[7:9])
     catch
         infties += 1
-        nD4AnomalyRatio[i] = Inf
+        AnomalyRatio[i] = Inf
     end
 end
-nD4AnomalyRatio = substitute(nD4AnomalyRatio, Dict([u1 => 1, u2 => 1, d1 => 1, e1 => 1]))
+infties
 
-function EoverN(u1, u2, d1, e1)
-    2//3 + 2 * simplify_fractions((2u1 +u2+3*e1) //(2u1+u2+3d1))
-end
+AnomalyRatio = substitute(AnomalyRatio, Dict([var => 1 for var in unique(model)]))
 
-nD4AnomalyRatio = Symbolics.value.(nD4AnomalyRatio)
+AnomalyRatio = Symbolics.value.(AnomalyRatio)
 
-end
+#end
 
-histogram(nD4AnomalyRatio)
+histogram(AnomalyRatio)
